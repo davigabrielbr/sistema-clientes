2e321cd6a5392fcf2b0a886ab966fcea48c686ea
Davi Gabriel <davi_gabriel99@hotmail.com>
2026-08-11 17:19:46 -0300

    refactor: remove credenciais do código

--- src/connection/Conexao.java
+++ src/connection/Conexao.java
@@ -3,6 +3,9 @@ package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Conexao {
     private static final String url = "jdbc:mysql://localhost:3306/sistema_clientes";
@@ -14,14 +17,33 @@ public class Conexao {
     public static Connection getConexao() {
         try {
             if (conn == null) {
+                Properties properties = carregarConfiguracao();
+
+                String url = properties.getProperty("db.url");
+                String user = properties.getProperty("db.user");
+                String password = properties.getProperty("db.password");
+
                 conn = DriverManager.getConnection(url, user, password);
-                return conn;
-            } else {
-                return conn;
             }
+
+            return conn;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private static Properties carregarConfiguracao() {
+        Properties properties = new Properties();
+
+        try {
+            FileInputStream arquivo = new FileInputStream("config.properties");
+            properties.load(arquivo);
+            arquivo.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
     }
 }
